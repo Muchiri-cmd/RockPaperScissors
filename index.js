92ebf5309aefcdddf85cb767ec9d96a6233f64c2
Muchiri-cmd <davismuchiri21@gmail.com>
2023-11-11 17:41:15 +0300

    Add computer choice feauture

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ const rock_div=document.getElementById("rock");
 const paper_div=document.getElementById("paper");
 const scissors_div=document.getElementById("scissors");
 
-rock_div.addEventListener("click",()=>{
-    console.log("You clicked on rock");
-})
+function getComputerChoice(){
+    const choices=['rock','paper','scissors'];
+    let randomNumber=Math.floor(Math.random()*3);
+    return choices[randomNumber]; 
+}
+
+function game(playerChoice){
+    //console.log(`$playerChoice`);
+}
+
+//main function
+function main(){
+    rock_div.addEventListener("click",()=>{
+        game("rock");
+    })
+    paper_div.addEventListener("click",()=>{
+        game("paper");
+    })
+    scissors_div.addEventListener("click",()=>{
+        game("paper");
+    })
+}
+
+main();
