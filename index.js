8d832079cef1e0d26cfbbf81e031d384156e5f72
Muchiri-cmd <davismuchiri21@gmail.com>
2023-11-11 17:20:16 +0300

    Fix placement of javascript link

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+"use strict";
+
+const playerScore=0;
+const computerScore=0;
+const playerScore_Span=document.getElementById("user-score");
+const computerScore_Span=document.getElementById("computer-score"); 
+const ccoreBoard_div=document.querySelector(".scoreboard");
+const result_div=document.querySelector(".result");
+const rock_div=document.getElementById("rock");
+const paper_div=document.getElementById("paper");
+const scissors_div=document.getElementById("scissors");
+
+rock_div.addEventListener("click",()=>{
+    console.log("You clicked on rock");
+})
